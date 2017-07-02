3432012b715d706ffcba7937f3fc45fef9dbd45d
Hernawan Fa'iz Abdillah <hernawan.faiz@abdillah.me>
2017-07-02 21:09:43 +0700

    Move compositor into separate module

---
+++ src/compositor.rs
@@ -0,0 +1,45 @@
+use wayland_server::{EventLoopHandle, GlobalHandler, Client};
+use wayland_server::protocol::wl_compositor;
+use wayland_server::protocol::wl_compositor::WlCompositor;
+use wayland_server::protocol::wl_surface::WlSurface;
+use wayland_server::protocol::wl_region::WlRegion;
+
+pub struct CompositorData {}
+
+impl CompositorData {
+    pub fn new() -> Self {
+        CompositorData {}
+    }
+}
+
+impl GlobalHandler<WlCompositor> for CompositorData {
+    fn bind(&mut self, evqh: &mut EventLoopHandle, client: &Client, global: WlCompositor) {
+        println!("global: Compositor registered");
+        // let compositor_hand_id = evqh.add_handler(CompositorData::new());
+        // evqh.register::<WlCompositor, CompositorData>(&global, compositor_hand_id);
+    }
+}
+
+impl wl_compositor::Handler for CompositorData {
+    fn create_surface(
+        &mut self,
+        evqh: &mut EventLoopHandle,
+        client: &Client,
+        resource: &WlCompositor,
+        id: WlSurface
+    ) {
+        println!("compositor: create surface");
+    }
+
+    fn create_region(
+        &mut self,
+        evqh: &mut EventLoopHandle,
+        client: &Client,
+        resource: &WlCompositor,
+        id: WlRegion
+    ) {
+        println!("compositor: create region");
+    }
+}
+
+declare_handler!(CompositorData, wl_compositor::Handler, wl_compositor::WlCompositor);
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,19 @@
+#[macro_use]
 extern crate wayland_server;
 
-use wayland_server::{EventLoopHandle, GlobalHandler, Client};
-use wayland_server::protocol::wl_compositor::{WlCompositor, Handler};
+use wayland_server::Client;
+use wayland_server::protocol::wl_compositor::WlCompositor;
 use wayland_server::protocol::wl_surface::WlSurface;
 use wayland_server::protocol::wl_region::WlRegion;
 
-struct CompositorData {
-    // Surface list
-}
-
-impl CompositorData {
-    pub fn new() -> Self {
-        CompositorData {}
-    }
-}
-
-impl GlobalHandler<WlCompositor> for CompositorData {
-    fn bind(&mut self, evqh: &mut EventLoopHandle, client: &Client, global: WlCompositor) {
-        unimplemented!()
-    }
-}
-
-impl wl_compositor::Handler for CompositorData {
-    fn create_surface(
-        &mut self,
-        evqh: &mut EventLoopHandle,
-        client: &Client,
-        resource: &WlCompositor,
-        id: WlSurface
-    ) {
-        unimplemented!()
-    }
-
-    fn create_region(
-        &mut self,
-        evqh: &mut EventLoopHandle,
-        client: &Client,
-        resource: &WlCompositor,
-        id: WlRegion
-    ) {
-        unimplemented!()
-    }
-}
+mod compositor;
 
 fn main() {
     let (mut display, mut event_loop) = wayland_server::create_display();
     display.add_socket_auto().expect("Display socket cannot be used");
 
-    let compositor_hand_id = event_loop.add_handler(CompositorData::new());
-    event_loop.register_global::<WlCompositor, CompositorData>(compositor_hand_id, /* version: */ 3);
+    let compositor_hand_id = event_loop.add_handler(compositor::CompositorData::new());
+    event_loop.register_global::<WlCompositor, compositor::CompositorData>(compositor_hand_id, /* version: */ 3);
 
     loop {
         // flush events to client sockets
